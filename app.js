516fe8762e0426e3c75b11cd9aa005ebd036cdab
priyanshu10y <priyanshuyadavweb@gmail.com>
2024-08-07 23:07:32 +0530

    finished

---
+++ app.js
@@ -0,0 +1,10 @@
+const emoji = document.querySelector("#emoji")
+
+const emojis = [
+    "😂", "😊", "🥰", "😎", "🤔", "😄", "🙃", "😍", "🤩", "😋",
+    "🥳", "😴", "🤯", "🥺", "😇", "🤠", "🤓", "🧐", "😜", "🤪", "😱"
+]
+
+emoji.addEventListener('mouseover', () => {
+  emoji.innerHTML = emojis[Math.floor(Math.random()*emojis.length)]
+})
